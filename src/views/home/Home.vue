a1464672c7d680814e7b0cadf058f104a7931945
ShibaInuOfMars <1051858676@qq.com>
2019-10-07 21:51:30 +0800

    首页制作

---
+++ src/views/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <HomeHeader />
+    </div>
+</template>
+
+<script>
+
+    // 主页依赖组件
+    import HomeHeader from './components/home-header/HomeHeader';
+
+    export default {
+        name: 'home',
+        components: {
+            HomeHeader
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/home/components/home-header/HomeHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="home-header">
+        <div class="search-box">输入城市/景点/游玩主题</div>
+        <div class="position">东莞</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHeader"
+    }
+</script>
+
+<style lang="less" scoped>
+    .home-header {
+        width: 100%;
+        height: 44rem / 50;
+        background-color: #00BCD4;
+        padding: 0 10rem / 50;
+        display: flex;
+        align-items: center;
+        font-size: 14rem / 50;
+
+        .search-box {
+            flex: 1;
+            height: 30rem / 50;
+            background-color: #fff;
+            border-radius: 3rem / 50;
+            display: flex;
+            align-items: center;
+            padding-left: 10rem / 50;
+            color: #E4E7EA;
+        }
+
+        .position {
+            width: 50rem / 50;
+            text-align: center;
+            margin-left: 5rem / 50;
+            color: #fff;
+        }
+    }
+</style>
