6522d145a75deb140b36a600746ffc97cf1bd83e
ShibaInuOfMars <1051858676@qq.com>
2019-10-07 20:58:24 +0800

    rem配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index';
 
+// 配置rem，以便适配不同的手机屏幕
+import './assets/common/index.less';
+
 Vue.config.productionTip = false;
 
 new Vue({
