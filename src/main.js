7f9d944c518d1bc265136757ca875e524e7f3d3e
ShibaInuOfMars <1051858676@qq.com>
2019-10-08 13:45:00 +0800

    引入fastclick.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import './assets/styles/border.css';
 // 配置rem，以便适配不同的手机屏幕
 import './assets/common/index.less';
 
+// 解决移动设备上的浏览器默认会在用户点击屏幕大约延迟300毫秒后才会触发点击事件
+import FastClick from 'fastclick';
+FastClick.attach(document.body);
+
 Vue.config.productionTip = false;
 
 new Vue({
