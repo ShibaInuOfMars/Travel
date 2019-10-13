3c7dda27c70accb02137fc20ec017aea6aa7fb5a
ShibaInuOfMars <1051858676@qq.com>
2019-10-13 13:46:02 +0800

    主页的中间功能区

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,16 @@
         <home-header />
         <home-swiper />
         <home-icons />
+        <div class="home-center-tool border-top border-color">
+            <div class="pos border-right border-color">
+                <span class="iconfont icon-pos">&#xe6e3;</span>
+                <span>定位失败</span>
+            </div>
+            <div class="ranking">
+                <span class="iconfont icon-dq">&#xe633;</span>
+                <span>必游榜单</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,5 +34,42 @@
 </script>
 
 <style lang="less" scoped>
+    .home {
 
+        .home-center-tool {
+            display: flex;
+            height: .98rem;
+            // background: red;
+            font-size: .3rem;
+
+            .border-color {
+                border-color: #CCCCCC;
+            }
+            
+            > div {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .pos {
+                flex: 1;
+
+                .icon-pos {
+                    color: #959595;
+                    font-size: .5rem;
+                }
+            }
+
+            .ranking {
+                flex: 1;
+
+                .icon-dq {
+                    color: #959595;
+                    font-size: .4rem;
+                    margin-right: .1rem;
+                }
+            }
+        }
+    }
 </style>
